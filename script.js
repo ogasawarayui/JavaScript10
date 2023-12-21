e7d88d74b5b70874664957b4cee57779bc3f1a65
ogasawarayui <ogasawara.yui@outlook.com>
2023-12-21 22:46:05 +0900

    js10

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 /* Q1 3つ以上の値を用意して変数に代入してください。その1番目と3番目をコンソールで表示してください。*/
 var fruits = ["リンゴ", "バナナ", "イチゴ"];
-console.log(fruits[0, 2]);
+console.log(fruits[0], fruits[2]);
 /* Q2 key、value、プロパティーについて説明してください。
 プロパティとは名前（キー）と値（バリュー）が対になったもの
 keyはデータセットを定義する定数（例：性別、色、価格など）
 valueは、そのセットに属する変数（例：男性／女性、緑、100 など）*/
 /* Q3 3つほどプロパティを用意したオブジェクトを用意してコンソールで表示してください。*/
-var friends = [
-  {
-    name: "merody",
-    subject: "rabbit",
-
-  },
-  {
-    name: "kuromi",
-    subject: "cat",
-
-  },
-  {
-    name: "piano",
-    subject: "sheep",
-  }
-]
-console.log(friends.name);
+var friends = {
+  name: "merody",
+  subject: "rabbit",
+  age: "24"
+}
+console.log(friends);
 /* Q4 オブジェクトを3つ用意してそれを配列として代入してください。配列の0番目と3番目を表示させ、その中のkeyを指定してコンソールに表示させてください。*/
 var friends = [
   {
@@ -40,4 +29,4 @@ var friends = [
     subject: "sheep",
   }
 ]
-console.log(friends.name[0, 2]);
+console.log(friends[0].name, friends[2].name);
